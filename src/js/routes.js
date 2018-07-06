b729ebd2576d62714a6730a98734f581bf6beefd
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-07-06 16:58:31 +0300

    move mobile buttons to separate function

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,64 +2,7 @@ import GameState from './gameObjects/gameState';
 import detectMobile from './utils/detectMobile';
 
 let content = document.querySelector('.content');
-
-if (detectMobile()) {
-
-    var divForButtons = document.createElement('div');
-    divForButtons.classList.add('buttons');
-
-    var buttonLeft = document.createElement('button');
-    buttonLeft.innerHTML = '<-';
-    buttonLeft.classList.add('left');
-
-    var buttonUp = document.createElement('button');
-    buttonUp.innerHTML = '^';
-    buttonUp.classList.add('up');
-
-    var buttonRight = document.createElement('button');
-    buttonRight.innerHTML = '->';
-    buttonRight.classList.add('right');
-
-    var buttonDown = document.createElement('button');
-    buttonDown.innerHTML = 'v';
-    buttonDown.classList.add('down');
-
-
-
-    divForButtons.appendChild(buttonLeft);
-    divForButtons.appendChild(buttonUp);
-    divForButtons.appendChild(buttonRight);
-    divForButtons.appendChild(buttonDown);
-
-    divForButtons.addEventListener('click', (event) => {
-        if (event.target.matches('.right')) {
-            let e = new Event('keydown');
-            e.keyCode = 39;
-            document.dispatchEvent(e);
-            return;
-        }
-        if (event.target.matches('.left')) {
-            let e = new Event('keydown');
-            e.keyCode = 37;
-            document.dispatchEvent(e);
-            return;
-        }
-        if (event.target.matches('.up')) {
-            let e = new Event('keydown');
-            e.keyCode = 38;
-            document.dispatchEvent(e);
-            return;
-        }
-        if (event.target.matches('.down')) {
-            let e = new Event('keydown');
-            e.keyCode = 40;
-            document.dispatchEvent(e);
-            return;
-        }
-    });
-
-    document.body.appendChild(divForButtons);
-}
+addButtonsForMobile();
 
 const routes = [
     {
@@ -97,6 +40,66 @@ const routes = [
 function changeActivePage(newPage) {
     document.querySelector('.active').className = '';
     document.querySelector(`[href="#${newPage}"]`).parentElement.className = 'active';
+}
+
+function addButtonsForMobile() {
+    if (detectMobile()) {
+
+        var divForButtons = document.createElement('div');
+        divForButtons.classList.add('buttons');
+
+        var buttonLeft = document.createElement('button');
+        buttonLeft.innerHTML = '<';
+        buttonLeft.classList.add('left');
+
+        var buttonUp = document.createElement('button');
+        buttonUp.innerHTML = '^';
+        buttonUp.classList.add('up');
+
+        var buttonRight = document.createElement('button');
+        buttonRight.innerHTML = '>';
+        buttonRight.classList.add('right');
+
+        var buttonDown = document.createElement('button');
+        buttonDown.innerHTML = 'v';
+        buttonDown.classList.add('down');
+
+
+
+        divForButtons.appendChild(buttonLeft);
+        divForButtons.appendChild(buttonUp);
+        divForButtons.appendChild(buttonRight);
+        divForButtons.appendChild(buttonDown);
+
+        divForButtons.addEventListener('click', (event) => {
+            if (event.target.matches('.right')) {
+                let e = new Event('keydown');
+                e.keyCode = 39;
+                document.dispatchEvent(e);
+                return;
+            }
+            if (event.target.matches('.left')) {
+                let e = new Event('keydown');
+                e.keyCode = 37;
+                document.dispatchEvent(e);
+                return;
+            }
+            if (event.target.matches('.up')) {
+                let e = new Event('keydown');
+                e.keyCode = 38;
+                document.dispatchEvent(e);
+                return;
+            }
+            if (event.target.matches('.down')) {
+                let e = new Event('keydown');
+                e.keyCode = 40;
+                document.dispatchEvent(e);
+                return;
+            }
+        });
+
+        document.body.appendChild(divForButtons);
+    }
 }
 
 export default routes;
