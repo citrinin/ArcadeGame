6f1b40a5d1e1f108d6e30fcc8ca76e2fcc08c4bb
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-07-06 16:58:50 +0300

    add motion to person

--- src/js/gameObjects/gameState.js
+++ src/js/gameObjects/gameState.js
@@ -2,22 +2,31 @@ import Hero from './hero';
 
 export default class GameState {
 	constructor() {
+		this.width = 800;
+		this.height = 800;
 		this.level = 1;
-		this.baseSpeed = 20;
+		this.baseSpeed = 5;
 		this.canvas = document.querySelector('canvas');
-		this.canvas.width = 800;
-		this.canvas.height = 800;
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
 
 		this.context = this.canvas.getContext('2d');
 
 		this.hero = new Hero(this);
+		this.gamePlays = false;
 		this.runGame();
+		this.timer = 0;
 	}
 
 	runGame() {
-		setInterval(() => {
-			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.gamePlays = true;
+		this.timer = setInterval(() => {
+			this.context.clearRect(0, 0, this.width, this.height);
 			this.context.drawImage(this.hero.getNextSprite(), this.hero.position.x, this.hero.position.y, 115, 100);
 		}, 30);
+	}
+	pauseGame() {
+		this.gamePlays = false;
+		clearInterval(this.timer);
 	}
 }
--- src/js/gameObjects/person.js
+++ src/js/gameObjects/person.js
@@ -1,5 +1,8 @@
 export default class Person {
-    constructor(gameState, selector) {
+    constructor(gameState, selector, mainHero) {
+        this.mainHero = mainHero;
+        this.game = gameState;
+        this.directionAngle = this.mainHero ? 0 : 2 * Math.PI * Math.random();
         this.position = {
             x: 0,
             y: 0
@@ -9,10 +12,27 @@ export default class Person {
         this.currentImg = 0;
     }
     getNextSprite() {
+        this.step();
         if (this.currentImg >= this.sprites.length) {
             this.currentImg = 0;
         }
         return this.sprites[this.currentImg++];
+    }
+    step() {
+        this.position.x += Math.cos(this.directionAngle) * this.speed;
+        if (this.position.x > this.game.width) {
+            this.position.x = 0;
+        }
+        if (this.position.x < 0) {
+            this.position.x = this.game.width;
+        }
+        this.position.y += Math.sin(this.directionAngle) * this.speed;
+        if (this.position.y > this.game.height) {
+            this.position.y = 0;
+        }
+        if (this.position.y < 0) {
+            this.position.y = this.game.height;
+        }
     }
 }
 
