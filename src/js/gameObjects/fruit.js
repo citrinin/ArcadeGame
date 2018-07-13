fdf0277f16196e533e707d1b98197ea153d21c8a
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-07-13 12:29:54 +0300

    change position of fruit appearance

--- src/js/gameObjects/fruit.js
+++ src/js/gameObjects/fruit.js
@@ -3,7 +3,11 @@ export default class Fruit {
         this.width = 30;
         this.height = 30;
         this.game = gameState;
-        this.generatePosition();
+        this.position = {
+            x: Math.abs(this.width - this.game.width * Math.random()),
+            y: this.game.height / 2 * Math.random()
+        };
+
         this.sprite = this.generateSptite();
     }
     getNextSprite() {
@@ -22,18 +26,6 @@ export default class Fruit {
         this.position.y = newPosition.y;
     }
 
-    generatePosition() {
-        do {
-            this.position = {
-                x: Math.abs(this.width - this.game.width * Math.random()),
-                y: this.game.height / 2 * Math.random()
-            };
-        } while (this.getDistanceToHero() < 200);
-    }
-    getDistanceToHero() {
-        let hero = this.game.hero;
-        return Math.sqrt(Math.pow(hero.position.x - this.position.x, 2) + Math.pow(hero.position.y - this.position.y, 2));
-    }
     generateSptite() {
         let sprites = document.querySelectorAll('.fruit-img');
         let fruitNumber = Math.round(Math.random() * (sprites.length - 1));
